a5340b82ac406caf6d55f429e27595c3915662d0
azure-leo <Felixculpa16>
2023-06-05 03:55:34 +0600

    hw0: 1

--- hw0/a.py
+++ hw0/a.py
@@ -1,1 +1,28 @@
-n = input()
+import sys
+from collections import defaultdict
+
+
+counter = defaultdict(int)
+max_count = 0
+
+for line in sys.stdin:
+	for c in line.strip():
+		if c != " ":
+			counter[c] += 1
+			max_count = max(max_count, counter[c])
+		
+
+items = sorted(counter.items())
+
+
+
+n = len(items)
+
+for ri in range(max_count, 0, -1):
+	ans = []
+	for i in range(0, n):
+		count = items[i][1]
+		ans.append('#' if ri <= count else ' ')
+	print(''.join(ans))
+	
+print(''.join(items[i][0] for i in range(n)))
